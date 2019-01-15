543217e517df288a6957b4dac16a9e863b46ea89
Polevoi Vadim <vadimka.polevoi@gmail.com>
2019-01-15 17:30:31 +0200

    Product page added

---
+++ js/slideshow.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.slide-box').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000  
+    });
+});
